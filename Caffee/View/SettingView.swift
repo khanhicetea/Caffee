4d9ea073d89c68860f585d3ba63df3e83e59e6bc
KhanhIceTea <khanhicetea@gmail.com>
2026-01-31 14:27:17 +0700

    v1.21.0

--- Caffee/View/SettingView.swift
+++ Caffee/View/SettingView.swift
@@ -5,81 +5,81 @@ import Settings
 import SwiftUI
 
 struct GeneralView: View {
-  @EnvironmentObject var appState: AppState
-  @Default(.checkForUpdatesAutomatically) var checkForUpdatesAutomatically
+    @EnvironmentObject var appState: AppState
+    @Default(.checkForUpdatesAutomatically) var checkForUpdatesAutomatically
 
-  let appVersion = Bundle.main.appVersionLong
+    let appVersion = Bundle.main.appVersionLong
 
-  var body: some View {
-    VStack {
-      Image("Cficon")
-        .resizable()
-        .aspectRatio(contentMode: .fit)
-        .clipShape(RoundedRectangle(cornerRadius: 8))
-        .padding([.bottom], 10)
+    var body: some View {
+        VStack {
+            Image("Cficon")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+                .padding([.bottom], 10)
 
-      Grid(
-        alignment: .leading,
-        horizontalSpacing: 20.0,
-        verticalSpacing: 15.0
-      ) {
-        GridRow {
-          Text("Bật / Tắt gõ TV")
-          Toggle("", isOn: $appState.enabled)
-            .toggleStyle(SwitchToggleStyle(tint: .accentColor))
-        }
+            Grid(
+                alignment: .leading,
+                horizontalSpacing: 20.0,
+                verticalSpacing: 15.0
+            ) {
+                GridRow {
+                    Text("Bật / Tắt gõ TV")
+                    Toggle("", isOn: $appState.enabled)
+                        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                }
 
-        GridRow {
-          Text("Mở lúc bật Mac")
-          LaunchAtLogin.Toggle("").toggleStyle(SwitchToggleStyle(tint: .accentColor))
-        }
-        GridRow {
-          Text("Kiểu Gõ").frame(alignment: .leading)
-          Picker("", selection: $appState.typingMethod) {
-            ForEach(TypingMethods.allCases, id: \.self) { method in
-              Text(method.rawValue)
+                GridRow {
+                    Text("Mở lúc bật Mac")
+                    LaunchAtLogin.Toggle("").toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                }
+                GridRow {
+                    Text("Kiểu Gõ").frame(alignment: .leading)
+                    Picker("", selection: $appState.typingMethod) {
+                        ForEach(TypingMethods.allCases, id: \.self) { method in
+                            Text(method.rawValue)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                GridRow {
+                    Text("Phụ âm z,w,j,f")
+                    Toggle("", isOn: $appState.allowedZWJF)
+                        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                }
+                GridRow {
+                    Text("Phím tắt")
+                    KeyboardShortcuts.Recorder("", name: .toggleInputMode)
+                }
+                GridRow {
+                    Text("Tự động cập nhật")
+                    Toggle("", isOn: $checkForUpdatesAutomatically)
+                        .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                }
+                //          GridRow {
+                //            Text("OpenAI Token \(Defaults[.token])")
+                //            SecureField("Token", text: $token)
+                //          }
             }
-          }.pickerStyle(.segmented)
-        }
-        GridRow {
-          Text("Phụ âm z,w,j,f")
-          Toggle("", isOn: $appState.allowedZWJF)
-            .toggleStyle(SwitchToggleStyle(tint: .accentColor))
-        }
-        GridRow {
-          Text("Phím tắt")
-          KeyboardShortcuts.Recorder("", name: .toggleInputMode)
-        }
-        GridRow {
-          Text("Tự động cập nhật")
-          Toggle("", isOn: $checkForUpdatesAutomatically)
-            .toggleStyle(SwitchToggleStyle(tint: .accentColor))
-        }
-        //          GridRow {
-        //            Text("OpenAI Token \(Defaults[.token])")
-        //            SecureField("Token", text: $token)
-        //          }
-      }
 
-      Text(
-        "Version : \(appVersion)\nKhông có tính năng gì ngoài gõ Tiếng Việt !\n Developed by [KhanhIceTea](https://khanhicetea.com)."
-      )
-      .multilineTextAlignment(.center)
-      .italic()
-      .padding([.vertical], 10)
+            Text(
+                "Version : \(appVersion)\nKhông có tính năng gì ngoài gõ Tiếng Việt !\n Developed by [KhanhIceTea](https://khanhicetea.com)."
+            )
+            .multilineTextAlignment(.center)
+            .italic()
+            .padding([.vertical], 10)
 
+        }
+        .frame(width: 270, height: 390)
+        .padding(10)
     }
-    .frame(width: 270, height: 390)
-    .padding(10)
-  }
 }
 
 struct GeneralView_Previews: PreviewProvider {
-  static var previews: some View {
-    GeneralView()
-      .environmentObject(AppState())
-      .previewLayout(PreviewLayout.sizeThatFits)
-      .padding()
-      .previewDisplayName("GeneralView preview")
-  }
+    static var previews: some View {
+        GeneralView()
+            .environmentObject(AppState())
+            .previewLayout(PreviewLayout.sizeThatFits)
+            .padding()
+            .previewDisplayName("GeneralView preview")
+    }
 }
